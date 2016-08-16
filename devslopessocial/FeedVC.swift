e2363c550ad017fb1b61f0137ee741991308e381
magonicolas <magonicolas@gmail.com>
2016-08-16 17:39:14 -0300

    Firebase Get Complete

--- devslopessocial/FeedVC.swift
+++ devslopessocial/FeedVC.swift
@@ -14,6 +14,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     
     @IBAction func signOutTapped(_ sender: AnyObject)
     {
@@ -30,8 +32,22 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: {(snapshot) in
-            print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot
+                {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject>
+                    {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
+        
 
     }
     
@@ -41,10 +57,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 3
+        return posts.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("Mago: \(post.caption)")
+        
         return (tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell)!
     }
 
